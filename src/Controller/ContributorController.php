9455d060ea79a4dcabfe2ab035a9697124c47c9c
nicolas <nicolas.leblay78@gmail.com>
2019-08-31 13:09:03 +0200

    CRUD Serie pour le contributor + bouton contributor dans la navbar
    crud sans Add & Delete. Pour le moment le contributor peut tout modifier dans la série, reste juste a sélectionner dans la vue les données qu'il peut modifier

--- src/Controller/ContributorController.php
+++ src/Controller/ContributorController.php
@@ -4,16 +4,84 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Serie;
+use App\Entity\User;
+use App\Entity\Season;
+use App\Form\UserType;
+use App\Entity\Episode;
+use App\Form\SerieType;
+use App\Form\SeasonType;
+use App\Form\EpisodeType;
 class ContributorController extends AbstractController
 {
+        // ----------- LISTE SERIE -----------------
+
     /**
-     * @Route("/contributor", name="contributor")
+     * @Route("/contributor/serie", name="contributor_serie_list")
+     * 
+     * Affiche la liste des series
      */
-    public function index()
+    public function contributorSerie()
     {
-        return $this->render('contributor/index.html.twig', [
-            'controller_name' => 'ContributorController',
+        //1 : Récupérer tous les serie
+        $repo = $this->getDoctrine()->getRepository(Serie::class);
+        $serie = $repo->findAll();
+
+
+        //2 : Afficher une vue (contributor/serie_list.html.twig), dans laquelle on va faire un dump() de tous les serie
+        return $this->render('contributor/serie_list.html.twig', [
+            'serie' => $serie
         ]);
     }
+
+
+    // -------------- FICHE SERIE --------------------
+
+    /**
+     * @Route("/contributor/serie_sheet/{id}", name="contributor_serie_sheet")
+     */
+    public function contributorSerieSheet($id)
+    {
+
+        $manager = $this->getDoctrine()->getManager();
+        $serie = $manager->find(Serie::class, $id);
+
+
+
+        return $this->render("contributor/serie_sheet.html.twig", [
+            'serie' => $serie,
+        ]);
+    }
+
+    // ---------------- UPDATE SERIE ------------------
+
+    /**
+     * @Route("/contributor/update/serie/{id}", name="contributor_update_serie")
+     *
+     */
+    public function contributorUpdateSerie($id, Request $request)
+    {
+
+        $manager = $this->getDoctrine()->getManager();
+        $serie = $manager->find(Serie::class, $id); //objet rempli
+
+        $form = $this->createForm(SerieType::class, $serie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $manager->persist($serie);
+
+            $manager->flush();
+
+            $this->addFlash('success', 'La serie n°' . $id . ' a bien été modifiée !');
+            return $this->redirectToRoute('contributor_serie_list');
+        }
+
+        return $this->render('contributor/update_serie.html.twig', [
+            'serieForm' => $form->createView()
+        ]);
+    }
+
 }
