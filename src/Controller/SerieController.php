4a0785d31db36a65020ad65a79478b5c41ae16ad
vmpb <vbrangoulo@gmail.com>
2019-09-02 23:03:37 +0200

    Ajout d'une fiche détail série lors du clic sur voir détails + redirect
    redirection home lors du clic sur le logo 
    tout le css est a faire sur la page details.html.Twig loul

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Serie;
 
 class SerieController extends AbstractController
 {
@@ -21,5 +24,19 @@ class SerieController extends AbstractController
     public function category()
     {
         return $this->render('serie/category.html.twig', []);
+    }
+
+    /**
+     * @Route("/serie/{id}", name="serie_detail")
+     */
+    public function detailSerie($id, Request $request) {
+        
+        $manager = $this-> getDoctrine() -> getManager();
+        $serie = $manager-> find(Serie::class, $id);
+
+        return $this->render('serie/detail.html.twig',[
+            
+            'serie' => $serie,
+        ]);
     }
 }
