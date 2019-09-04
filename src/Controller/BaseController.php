22689a38a1f8d9531eae6625cb7e42f0d66c1f32
Alexandre <alexandre.lutz@hotmail.fr>
2019-09-04 11:14:12 +0200

    formaulaire dans footerController + creation ContactType

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -6,9 +6,8 @@ use App\Entity\Serie;
 use App\Entity\Commentary;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use App\Form\ContactType;  
-
+ 
+    
 class BaseController extends AbstractController
 {
     /**
@@ -46,66 +45,5 @@ class BaseController extends AbstractController
 
     
 	
-	/**
-	*
-	* @Route("/contact", name="contact")
-	*
-	*
-	*/
-	public function contact(Request $request, \Swift_Mailer $mailer){
-		
-		$form = $this -> createForm(ContactType::class, null, array());
-		$form -> handleRequest($request);
-		
-		// traitement des infos du formulaire
-		
-		if($form -> isSubmitted() && $form -> isValid()){
-			
-			$data = $form -> getData();
-			// permet de récupérer toutes les infos du formulaire
-			// prenom = $data['prenom']
-			// objet = $data['objet']
-			
-			if($this -> sendEmail($data, $mailer)){
-				// $mailer : objet swiftmailer
-				$this -> addFlash('success', 'Votre email a été envoyé et sera traité dans les meilleurs délais.');
-			}
-			else{
-				$this -> addFlash('errors', 'Un problème a eu lieu durant l\'envoie, veuillez ré-essayer plus tard');
-			}
-		}
-		
-		// Affichage de la vue
-		
-		return $this -> render('base/contact.html.twig', [
-			'contactForm' => $form -> createView()
-		]);
-	}
 	
-	/**
-	* Permet d'envoyer des emails
-	*
-	*/
-	public function sendEmail($data, \Swift_Mailer $mailer){
-		$mail = new \Swift_Message();
-		// On instancie un objet swiftmailer en précisant l'objet (sujet) du mail. 
-		
-		$mail 
-			-> setSubject('Envoyé par boutique: ' . $data['objet'])
-			-> setFrom($data['email'])
-			-> setTo('contact@boutique.com')
-			-> setBody(
-				$this -> renderView('mail/contact_mail.html.twig', [
-					'data' => $data
-				]), 'text/html'
-			)
-		;
-		
-		if($mailer -> send($mail)){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}	
 }
--- src/Controller/FooterController.php
+++ src/Controller/FooterController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ContactType; 
 
 class FooterController extends AbstractController
 {
@@ -25,14 +27,6 @@ class FooterController extends AbstractController
         return $this->render('footer/mentionslegales.html.twig', []);
     }
 
-    
-     /**
-     * @Route("/contactsupport", name="contactsupport")
-     */
-    
-    public function contactsupport() {
-        return $this->render('footer/contactsupport.html.twig', []);
-    }
      /**
      * @Route("/donneesperso", name="donneesperso")
      */
@@ -54,4 +48,68 @@ class FooterController extends AbstractController
     public function souteneznous() {
         return $this->render('footer/souteneznous.html.twig', []);
     }
+
+
+    /**
+	*
+	* 
+     * @Route("/contactsupport", name="contactsupport")
+     */
+
+	public function contact(Request $request, \Swift_Mailer $mailer){
+		
+		$form = $this -> createForm(ContactType::class, null, array());
+		$form -> handleRequest($request);
+		
+		// traitement des infos du formulaire
+		
+		if($form -> isSubmitted() && $form -> isValid()){
+			
+			$data = $form -> getData();
+			// permet de récupérer toutes les infos du formulaire
+			// prenom = $data['prenom']
+			// objet = $data['objet']
+			
+			if($this -> sendEmail($data, $mailer)){
+				// $mailer : objet swiftmailer
+				$this -> addFlash('success', 'Votre email a été envoyé et sera traité dans les meilleurs délais.');
+			}
+			else{
+				$this -> addFlash('errors', 'Un problème a eu lieu durant l\'envoie, veuillez ré-essayer plus tard');
+			}
+		}
+		
+		// Affichage de la vue
+		
+		return $this->render('footer/contactsupport.html.twig',  [
+			'contactForm' => $form -> createView()
+		]);
+	}
+	
+	/**
+	* Permet d'envoyer des emails
+	*
+	*/
+	public function sendEmail($data, \Swift_Mailer $mailer){
+		$mail = new \Swift_Message();
+		// On instancie un objet swiftmailer en précisant l'objet (sujet) du mail. 
+		
+		$mail 
+			-> setSubject('Envoyé par boutique: ' . $data['objet'])
+			-> setFrom($data['email'])
+			-> setTo('contact@boutique.com')
+			-> setBody(
+				$this -> renderView('mail/contact_mail.html.twig', [
+					'data' => $data
+				]), 'text/html'
+			)
+		;
+		
+		if($mailer -> send($mail)){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}	
 }
