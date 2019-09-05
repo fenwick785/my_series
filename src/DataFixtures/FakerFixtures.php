11328297b8f3005d4b036564f6027b7888713a5d
Alexandre <alexandre.lutz@hotmail.fr>
2019-09-05 16:44:26 +0200

    fixtures amelio

--- src/DataFixtures/FakerFixtures.php
+++ src/DataFixtures/FakerFixtures.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\DataFixtures;
- 
+
+
 use Faker;
 
 use App\Entity\User;
+use App\Entity\Serie;
+use App\Entity\Season;
+use App\Entity\Episode;
+use App\Entity\Category;
 use App\Entity\Commentary;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -16,8 +21,59 @@ class FakerFixtures extends Fixture
  
         // On configure dans quelles langues nous voulons nos données
         $faker = Faker\Factory::create('fr_FR');
+
+        /*tableau de nos categories*/
+
+        $categories = ['action', 'aventure' , 'science-fiction' , 'drame', 'biopics', 'fantastique', 'anim', 'comédie' ,'historique', 'espionnage','thriller', 'comédie musicale', 'horreur', 'politique' ];
+
+        /* On génere des categories dans la table catégories */
+
+        foreach ( $categories as $categorie) 
+        {
+            $cat = new Category;
+            $cat->setType($categorie);
+            $manager->persist($cat);
+        }
+
+        $manager->flush();
+
+        /* On recupere la liste de nos series saisons, episode pour leurs atribuer une ou plusieurs prorpiétés*/
+
+        $repository = $manager ->getRepository(Serie::class);
+        $series = $repository ->findAll();
+        $countS = count($series);
+
+        $repository = $manager->getRepository(Season::class);
+        $seasons = $repository ->findAll();
+        $countSea = count($seasons);
+
+        $repository = $manager->getRepository(Episode::class);
+        $episodes = $repository ->findAll();
+        $countE = count($episodes);
+
+        /*attribution des categories à serie*/
+
+        foreach($series as $serie)
+        {   
+
+            $counter = rand(0,5);
+
+            for($i=0; $i <= $counter; $i++){
+                $randCat = $faker->randomElement($categories);
+                if (!in_array($randCat,$serie->getCategories()))
+                {
+                    $serie->addCategory($randCat);
+                    $manager->persist($serie);
+                }
+            }
+        }
+
+        $manager->flush();
+
+       
+                    
  
-        // on créé 10 personnes
+        // on créé 50 personnes
         for ($i = 0; $i < 50; $i++) 
         {
             $user = new User();
@@ -31,34 +87,66 @@ class FakerFixtures extends Fixture
             $user -> setBirthDate($faker -> dateTimeThisCentury($max = 'now', $timezone = null));
             $user -> setNewsletter($faker -> boolean);
             $manager -> persist($user);
+        }
 
+        $manager->flush();
 
+         /* On recupere la liste de nos membres pour leurs atribuer une ou plusieurs commentaires*/
 
-            $limit = rand(0,5);
+         $repository = $manager ->getRepository(User::class);
+         $users = $repository ->findAll();
+         $countU = count($users);
 
-            for ($j = 0;$j < $limit; $j++)
+        $limit = rand(0,5);
+
+        foreach ($users as $user)
+        {
+
+            $counter= rand(0,4  );
+
+            for ($i=0 ; $i <= $counter ; $i++)
             {
-                $comment = new Commentary;
-                $comment -> setIdUser($user);
-                if($faker -> boolean == 0)
-                {
-                    $comment -> setRating(NULL);
-                }
-                else
-                {
-                    $comment -> setRating($faker -> boolean);
-                }
-                $comment -> setSpoil($faker -> boolean);
-                
-                $comment -> setContent($faker ->  text($maxNbChars = 200));
-                $manager -> persist($comment);
+
+            $comment = new Commentary;
+            $comment -> setIdUser($user);
+            $counterItem = rand (0, 2);
+
+
+            if ($counterItem == 0)
+            {
+                $comment -> setIdSerie($series[rand(0, $countS)]);
             }
+            elseif ($counterItem == 1)
+            {
+                $comment -> setIdSeason($seasons[rand(0,$countSea)]);
+            }
+            else
+            {
+                $comment -> setIdEpisode($episodes[rand(0,$countE)]);
+            }
+
+
+
+            if($faker -> boolean == 0)
+            {
+                $comment -> setRating(NULL);
+            }
+            else
+            {
+                $comment -> setRating($faker -> boolean);
+            }
+            $comment -> setSpoil($faker -> boolean); 
+            $comment -> setContent($faker ->  text($maxNbChars = 200));
+
+            $manager -> persist($comment);
+            }
+        }
 
         $manager->flush();
 
 
-        }
     }
 }
+
 
 ?>
